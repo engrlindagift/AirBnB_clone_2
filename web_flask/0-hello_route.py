44b6f8054050bfb197610fd1fcb0bb6cefa684e9
engrlindagift <lindagift0812@gmail.com>
2023-05-23 17:17:05 +0000

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""a script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_world():
+    """A funtion that returns HBNB"""
     return 'Hello, HBNB!'
 
 if __name__  == '__main__':
